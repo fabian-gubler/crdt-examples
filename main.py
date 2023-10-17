b46b76d8cf379fefef26882145c3a9aa7d865013
Fabian Gubler <fabian.gubler@hotmail.com>
2023-10-17 17:52:45 +0200

    handle multi node setup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from utils.node_id import generate_node_id
 from utils.network import SimpleNetwork
 from utils.node import Node
+from utils.node_manager import NodeManager
 from crdts.gcounter import GCounter
 from crdts.pncounter import PNCounter
 from crdts.lwwregister import LWWRegister
@@ -8,50 +9,94 @@ from crdts.lwwregister import LWWRegister
 
 def main():
     network = SimpleNetwork()
-    node_id = generate_node_id()
-    node = Node(node_id, network)
-
-    gcounter = GCounter(node_id)
-    pncounter = PNCounter(node_id)
-    lwwregister = LWWRegister(node_id)
+    manager = NodeManager(network)
 
     while True:
-        print(
-            "\nOptions: increment_gc, increment_pn, decrement_pn, set_lww, get_gc, get_pn, get_lww, send_state, receive_state, exit"
-        )
+        print("\nOptions: create_node, list_nodes, select_node, exit")
+        choice = input("Choose an action: ")
 
+        if choice == "create_node":
+            node_id = generate_node_id()
+            manager.create_node(node_id)
+            print(f"Node {node_id} created.")
+        elif choice == "select_node":
+            node_id_input = input("Enter node ID to select: ")
+            try:
+                node_id = int(node_id_input)
+                if node_id in manager.nodes:
+                    node_interaction(manager.nodes[node_id])
+                else:
+                    print("Invalid node ID.")
+            except ValueError:
+                print("Please enter a valid integer for node ID.")
+        elif choice == "list_nodes":
+            nodes = manager.list_nodes()
+            if nodes:
+                # Convert node IDs to strings before joining
+                print("Created nodes:", ", ".join(map(str, nodes)))
+            else:
+                print("No nodes created yet.")
+        elif choice == "exit":
+            break
+
+def node_interaction(node):
+    while True:
+        print(f"\nOptions for node {node.node_id}:")
+        print("increment_gc, increment_pn, decrement_pn, set_lww, get_gc, get_pn, get_lww, send_state, receive_state, back")
         choice = input("Choose an action: ")
 
         if choice == "increment_gc":
-            gcounter.increment()
+            node.gcounter.increment()
             print("GCounter incremented.")
+
         elif choice == "increment_pn":
-            pncounter.increment()
+            node.pncounter.increment()
             print("PNCounter incremented.")
+
         elif choice == "decrement_pn":
-            pncounter.decrement()
+            node.pncounter.decrement()
             print("PNCounter decremented.")
+
         elif choice == "set_lww":
             value = int(input("Enter a number to set in LWWRegister: "))
-            lwwregister.set(value)
+            node.lwwregister.set(value)
             print(f"LWWRegister set to {value}.")
+
         elif choice == "get_gc":
-            print(f"GCounter value: {gcounter.value()}")
+            print(f"GCounter value: {node.gcounter.get()}")
+
         elif choice == "get_pn":
-            print(f"PNCounter value: {pncounter.value()}")
+            print(f"PNCounter value: {node.pncounter.get()}")
+
         elif choice == "get_lww":
-            print(f"LWWRegister value: {lwwregister.get()}")
+            print(f"LWWRegister value: {node.lwwregister.get()}")
+
         elif choice == "send_state":
             crdt_type = input("Which CRDT state to send? (gcounter, pncounter, lwwregister): ")
-            node.send_state(crdt_type)
-            print(f"{crdt_type} state sent.")
+            receiver_id = input("Enter receiver node ID: ")
+            node.send_state(crdt_type, receiver_id)
+            print(f"{crdt_type} state sent to node {receiver_id}.")
+
         elif choice == "receive_state":
-            node.receive_state()
-            print("State received and merged.")
-        elif choice == "exit":
+            message = node.network.receive(node.node_id)
+            if message:
+                sender, (operation, data) = message
+                if operation == "gcounter_state":
+                    node.gcounter.merge(data)
+                elif operation == "pncounter_state":
+                    node.pncounter.merge(data)
+                elif operation == "lwwregister_op":
+                    node.lwwregister.apply_operation(data)
+                print(f"State received and merged from node {sender}.")
+            else:
+                print("No messages for this node.")
+
+        elif choice == "back":
             break
+
         else:
             print("Invalid choice.")
+
 
 
 if __name__ == "__main__":
