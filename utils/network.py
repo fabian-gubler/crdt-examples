48501302357b574f24d771cbb9efa4e820bf6873
Fabian Gubler <fabian.gubler@hotmail.com>
2023-10-17 17:52:09 +0200

    fix dict bug

--- utils/network.py
+++ utils/network.py
@@ -1,11 +1,14 @@
 class SimpleNetwork:
     def __init__(self):
-        self.queue = []
+        self.queues = {}
 
-    def send(self, message):
-        """Simulate sending a message by adding it to a queue."""
-        self.queue.append(message)
+    def register_node(self, node_id):
+        self.queues[node_id] = []
 
-    def receive(self):
-        """Simulate receiving a message by popping it from the queue."""
-        return self.queue.pop(0) if self.queue else None
+    def send(self, sender_id, receiver_id, message):
+        self.queues[receiver_id].append((sender_id, message))
+
+    def receive(self, node_id):
+        if self.queues[node_id]:
+            return self.queues[node_id].pop(0)
+        return None
