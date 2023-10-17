d39b6814a78794dfb4fafb760f2a502ba750a01e
Fabian Gubler <fabian.gubler@hotmail.com>
2023-10-17 17:52:19 +0200

    extend code

--- utils/node.py
+++ utils/node.py
@@ -7,25 +7,46 @@ class Node:
     def __init__(self, node_id, network):
         self.node_id = node_id
         self.network = network
+
+        # Initializing CRDTs
         self.gcounter = GCounter(node_id)
         self.pncounter = PNCounter(node_id)
         self.lwwregister = LWWRegister(node_id)
 
-    def send_state(self, crdt_type):
+    def send_state(self, crdt_type, receiver_id):
+        message = None
+
         if crdt_type == "gcounter":
-            message = Message(self.node_id, "gcounter_state", self.gcounter)
+            message = ("gcounter_state", self.gcounter.get_state())
         elif crdt_type == "pncounter":
-            message = Message(self.node_id, "pncounter_state", self.pncounter)
+            message = ("pncounter_state", self.pncounter.get_state())
         elif crdt_type == "lwwregister":
-            message = Message(self.node_id, "lwwregister_op", {"value": self.lwwregister.get(), "version_vector": self.lwwregister.version_vector})
-        self.network.send(message)
+            message_content = {
+                "value": self.lwwregister.get(),
+                "version_vector": self.lwwregister.version_vector.get_state()
+            }
+            message = ("lwwregister_op", message_content)
+
+        if message:
+            self.network.send(self.node_id, receiver_id, message)
+        else:
+            print(f"Invalid CRDT type: {crdt_type}")
 
     def receive_state(self):
-        message = self.network.receive()
-        if message:
-            if message.operation == "gcounter_state":
-                self.gcounter.merge(message.data)
-            elif message.operation == "pncounter_state":
-                self.pncounter.merge(message.data)
-            elif message.operation == "lwwregister_op":
-                self.lwwregister.apply_operation(message)
+        data = self.network.receive(self.node_id)
+        if not data:
+            print("No message available.")
+            return
+
+        sender_id, message = data
+        operation, content = message
+
+        if operation == "gcounter_state":
+            self.gcounter.merge(content)
+        elif operation == "pncounter_state":
+            self.pncounter.merge(content)
+        elif operation == "lwwregister_op":
+            # Assuming the LWWRegister's apply_operation method takes in the value and version vector
+            self.lwwregister.apply_operation(content["value"], content["version_vector"])
+        else:
+            print(f"Invalid operation received: {operation}")
